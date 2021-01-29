7c76cf22ff0bec09df7a23d56a489efadb989a91
Jonathan-Majors <izzymajors@gmail.com>
2021-01-29 11:59:53 -0600

    homepage has a header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {Route, Link, Switch } from 'react-router-dom'
+
+import FormHome from "./components/FormHome";
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+
+    <div className='container'>
+    <header><h1>Friends App</h1></header>
+
+    <FormHome
+      
+    />
+      
+
+
+
+
+
+     {/* <div className="App">
+    <nav>
+      <h1 className="store-header">Pizza to go</h1>
+    <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/order-pizza">Order here</Link>
+
+    </div>
+    </nav>
+
+    <Switch>
+      <Route path={"/components/FormHome"}>
+      
+      </Route>
+   </Switch>  */}
+
+   </div> 
   );
 };
 export default App;
---
+++ src/components/FormHome.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useHistory } from 'react-router-dom';
+
+
+
+export default function FormHome() {
+
+    const history = useHistory();
+
+    const routeToShop = () => {
+        console.log(history);
+        history.push("/order-pizza");
+    };
+
+    return (
+
+         <div className= 'home-wrapper'>
+             <img
+             className="home-image"
+             src= "https://cheeseboardcollective.coop/wp-content/uploads/2016/11/banner-pizza.jpg"
+             alt=""
+             />
+        
+        <button onClick={routeToShop} className= "md-button shop button">
+           Place Order Here
+        </button>
+          </div>
+    )
+
+}
