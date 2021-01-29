746c77afcbc35dbda4c772f14208896961ec4cae
Jonathan-Majors <izzymajors@gmail.com>
2021-01-29 13:05:01 -0600

    added toppings

--- src/components/FormHome.js
+++ src/components/FormHome.js
@@ -13,6 +13,19 @@ export default function FormHome(props) {
         errors,
       } = props
 
+      const onSubmit = evt => {
+        evt.preventDefault()
+        submit()
+      }
+    
+      const onChange = evt => {
+        /* 🔥 FIX THIS SO IT ALSO WORKS WITH CHECKBOXES */
+        const { name, value, type, checked } = evt.target
+        const valueToUse = type === 'checkbox' ? checked : value;
+        change(name, valueToUse);
+      }
+
+
     const history = useHistory();
 
     const routeToShop = () => {
@@ -32,9 +45,80 @@ export default function FormHome(props) {
         <button onClick={routeToShop} className= "md-button shop button">
            Place Order Here
         </button>
-          </div>
+   
+          
+          
+          <label>size
+              <select
+              onChange={onChange}
+              //value={value.size}
+              name='size'
+              >
+                  <option value=''>--select an option</option>
+                  <option value=''>small</option>
+                  <option value=''>medium</option>
+                  <option value=''>Large</option>
+                </select>
+              </label>
+
+             
+
+             <div className='form-group checkboxes'>
+                 <h4>Toppings</h4>
+
+                 <label>sausage
+                     <input
+                     type='checkbox'
+                     name='sausage'
+                    // checked={value.sausage}
+                     onChange={onChange}
+                     />
+
+                 </label>
+
+                 <label>sausage
+                     <input
+                     type='checkbox'
+                     name='sausage'
+                    // checked={value.sausage}
+                     onChange={onChange}
+                     />
+
+                 </label>
+
+                 <label>peperoni
+                     <input
+                     type='checkbox'
+                     name='peperoni'
+                    // checked={value.sausage}
+                     onChange={onChange}
+                     />
+
+                 </label>
+
+                 <label>cheese
+                     <input
+                     type='checkbox'
+                     name='sausage'
+                    // checked={value.sausage}
+                     onChange={onChange}
+                     />
+
+                 </label>
+
+                 <label>onions
+                     <input
+                     type='checkbox'
+                     name='onions'
+                    // checked={value.sausage}
+                     onChange={onChange}
+                     />
+
+                 </label>
 
 
+             </div>
+               </div>
     )
 
 }
